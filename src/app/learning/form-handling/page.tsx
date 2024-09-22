808c6dd63ce81ccc07ca2afb3275cb2da91b4de2
duongacy <duongy96@gmail.com>
2024-09-22 20:25:26 +0700

    update image upload

--- src/app/learning/form-handling/page.tsx
+++ src/app/learning/form-handling/page.tsx
@@ -1,103 +1,58 @@
 'use client'
-import { solidButtonClassName } from '@/dp__atoms/Button'
-import { api } from '@/services/axios'
-import { URLS } from '@/services/urls'
+import { ImageUpload } from '@/dp__atoms/ImageUpload'
+import { useUploadImageMutation } from '@/services/media/upload-image/mutation'
 import { cn } from '@/utils/cn'
-import React, { useEffect } from 'react'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { SubmitHandler, useForm } from 'react-hook-form'
+import zod from 'zod'
 
-type FileWithPreview = File & { previewUrl?: string }
+const formPayloadSchema = zod.object({
+  username: zod.string({ message: 'Username is required' }),
+  avatar: zod.string({ message: 'Avatar is required' }),
+})
+type FormPayload = zod.infer<typeof formPayloadSchema>
 
 export default function Page() {
-  const [file, setFile] = React.useState<FileWithPreview | null>(null)
-  const [payload, setPayload] = React.useState<{
-    username: string
-    avatar: string
-  }>({ avatar: '', username: '' })
+  const formMethods = useForm<FormPayload>({
+    values: formPayloadSchema.parse({ username: '', avatar: '' }),
+    resolver: zodResolver(formPayloadSchema),
+  })
 
-  useEffect(() => {
-    return () => {
-      file?.previewUrl && URL.revokeObjectURL(file.previewUrl)
-    }
-  }, [file])
-
-  const handleFileChange = async (
-    event: React.ChangeEvent<HTMLInputElement>,
-  ) => {
-    if (!!event.target?.files?.length) {
-      const newFile = event.target.files![0]
-      setFile({
-        ...newFile,
-        previewUrl: URL.createObjectURL(newFile),
+  const uploadImageMuatation = useUploadImageMutation()
+  const changeFile = async (_name: string, blobFile: Blob) => {
+    uploadImageMuatation
+      .mutateAsync(blobFile)
+      .then((rs) => {
+        formMethods.setValue('avatar', rs.data?.url || '')
       })
-
-      const blob = new Blob([newFile as BlobPart], {
-        type: 'application/octet-stream',
-      })
-
-      setPayload({ ...payload, avatar: '' })
-      // Upload image to server and get the url
-      api
-        .upload(URLS.UPLOAD_IMAGE, blob)
-        .then((rs) => {
-          setPayload({ ...payload, avatar: rs.data.url })
-        })
-        .catch((error) => {
-          setFile(null)
-        })
-    }
+      .catch(() => {})
   }
 
-  const handleSubmit = (e) => {
-    e.preventDefault()
-    console.log(payload)
+  const handleSubmit: SubmitHandler<FormPayload> = (data) => {
+    console.log(data)
   }
 
   return (
     <div>
       <form
-        onSubmit={handleSubmit}
+        {...formMethods}
+        onSubmit={formMethods.handleSubmit(handleSubmit)}
         className="mx-auto mt-8 flex w-[400px] flex-col gap-8 border p-8"
       >
         <p className="text-center text-3xl font-bold">Form handling</p>
         <div className="grid grid-cols-[auto_1fr] items-center gap-x-4 gap-y-8">
           <span>Username:</span>
-          <input
-            type="text"
-            value={payload.username}
-            onChange={(e) =>
-              setPayload((p) => ({ ...p, username: e.target.value }))
-            }
-          />
+          <input type="text" {...formMethods.register('username')} />
           <span>Avatar:</span>
-          <label className="flex min-h-10 items-center border p-2">
-            <input
-              type="file"
-              name="name"
-              onChange={handleFileChange}
-              className="hidden"
-            />
-            {file?.previewUrl ? (
-              <img
-                src={file?.previewUrl}
-                alt=""
-                className={cn('opacity-30', {
-                  'opacity-100': !!payload.avatar,
-                })}
-              />
-            ) : (
-              'Select file'
-            )}
-          </label>
+          <ImageUpload
+            onChangeFile={changeFile}
+            isPending={uploadImageMuatation.isPending}
+            isSuccess={uploadImageMuatation.isSuccess}
+            isError={uploadImageMuatation.isError}
+            {...formMethods.register('avatar')}
+          />
         </div>
-        <button
-          type="submit"
-          className={cn(
-            solidButtonClassName({
-              disabled: !payload.username || !payload.avatar,
-            }),
-            'self-end',
-          )}
-        >
+        <button type="submit" className={cn('self-end')}>
           Submit
         </button>
       </form>
---
+++ src/design-pattern/atoms/ImageUpload.tsx
@@ -0,0 +1,89 @@
+'use client'
+import { cn } from '@/utils/cn'
+import {
+  DetailedHTMLProps,
+  InputHTMLAttributes,
+  useEffect,
+  useState,
+} from 'react'
+
+interface ImageUploadProps
+  extends DetailedHTMLProps<
+    InputHTMLAttributes<HTMLInputElement>,
+    HTMLInputElement
+  > {
+  label?: string
+  onChangeFile?: (fileName: string, blobFile: Blob) => void
+  isPending?: boolean
+  isError?: boolean
+  isSuccess?: boolean
+}
+
+export type FileWithPreview = File & {
+  preview: string
+}
+
+export const ImageUpload = ({
+  type,
+  label,
+  onChange,
+  onChangeFile,
+  isPending,
+  isError,
+  isSuccess,
+  ...props
+}: ImageUploadProps) => {
+  const [file, setFile] = useState<FileWithPreview | null>(null)
+
+  useEffect(() => {
+    return () => {
+      if (file?.preview) {
+        URL.revokeObjectURL(file.preview)
+      }
+    }
+  }, [file])
+
+  const handleChangeImage = (e) => {
+    if (e.target.files.length === 0) return
+    const newFile: FileWithPreview = e.target.files[0]
+    newFile.preview = URL.createObjectURL(newFile)
+    setFile(newFile)
+
+    const blob = new Blob([newFile as BlobPart], {
+      type: 'application/octet-stream',
+    })
+
+    onChangeFile?.(newFile.name, blob)
+  }
+
+  return (
+    <div>
+      {label && <label>{label}</label>}
+      <label
+        className={cn(
+          'relative flex aspect-square h-48 overflow-hidden border border-dashed border-gray-500',
+          {
+            'opacity-40': isPending,
+            'border-red-500': isError,
+            'border-green-500': isSuccess,
+          },
+        )}
+      >
+        {file?.preview && (
+          <img src={file.preview} className="absolute inset-0 object-fill" />
+        )}
+        {file?.name ? (
+          <span>{file?.name}</span>
+        ) : (
+          <span>Please select image</span>
+        )}
+        <input
+          type="file"
+          {...props}
+          className="hidden"
+          onChange={handleChangeImage}
+        />
+      </label>
+    </div>
+  )
+}
